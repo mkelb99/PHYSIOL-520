bb1a312faffeb9eefb9e3b8ad9c94eb9779f5839
mkelb99 <mkelb@umich.edu>
2023-01-19 11:18:17 -0500

    Completed Part B of Exercise 3.3

--- Exercise3_3.py
+++ Exercise3_3.py
@@ -17,31 +17,49 @@ def my_Gaussian(x):
 
 D = 0.32e-6
 M = 0.115
-t = 3000
+#t = 3000
 
 #plots for various values of time (part A)
-times = np.linspace(600,6000,25)
-X = np.linspace(-0.35,0.35,500)
+#times = np.linspace(600,6000,25)
 
-#make an empty list
-peaklist=[]
+#X = np.linspace(-0.35,0.35,500)
+
+##make an empty list
+#peaklist=[]
+
+# for t in times:
+#   Y = my_Gaussian(X)
+#   peak=Y.max()
+#   peaklist.append(peak)
+#   plt.subplot(1,2,1)
+#   plt.plot(X,Y)
+#   plt.xlabel('x (cm)')
+#   plt.ylabel('Concentration')
+
+# plt.subplot(1,2,2)
+# plt.plot(times,peaklist)
+# plt.xlabel('Time (Seconds)')
+# plt.ylabel('Peak Concentration Value')
+# plt.show()
 
 
+#Part B - measure of spread by eye
+times=np.array([600,6000,10000])
+X=np.linspace(-0.35,0.35,500)
 for t in times:
-  Y = my_Gaussian(X)
-  peak=Y.max()
-  peaklist.append(peak)
-  plt.subplot(1,2,1)
-  plt.plot(X,Y)
-  plt.xlabel('x (cm)')
-  plt.ylabel('Concentration')
-
+    Y = my_Gaussian(X)
+    plt.subplot(1,2,1)
+    plt.plot(X,Y)
+    plt.xlabel('x (cm)')
+    plt.ylabel('Concentration')
+    ##increase x resolution to better estimate
+    #plt.xticks(np.linspace(-.4,.4,10))
+    
+#estimating between midpoint of max in min to get inflection based on visuals
+inflection=np.array([.03*2,.08*2,.13*2])
 plt.subplot(1,2,2)
-plt.plot(times,peaklist)
-plt.xlabel('Time (Seconds)')
-plt.ylabel('Peak Concentration Value')
+plt.plot(times,inflection)
+plt.xlabel('Time (seconds)')
+plt.ylabel('Width')
 plt.show()
-
-#Part B 
-
 
